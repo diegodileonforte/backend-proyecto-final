67694cbe0021a74529ed25a73b49940a5bcbc7a8
Diego Di Leonforte <dileonfortediego@gmail.com>
2021-05-12 19:04:06 -0300

    Primera entrega proyecto final

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+const productRoutes = require('../routes/productos')
+const cartRoutes = require('../routes/carrito')
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public')))
@@ -9,23 +10,14 @@ app.use(express.urlencoded({ extended: true }))
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
-
 app.get('/', (req, res) => { res.render('../views/index'); })
 
 
-const productRoutes = require('../routes/productos');
 app.use('/productos', productRoutes)
-
-const cartRoutes = require('../routes/carrito');
 app.use('/carrito', cartRoutes)
 
 const PORT = 8080
-
 const server = app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${server.address().port}`)
 })
-
 server.on('error', error => console.log(`Error en servidor ${error}`))
